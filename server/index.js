c9f2225c4b9f54eabaf1bd0c987e9a0aeedfd311
chao lobi <gogoiladitya@gmail.com>
2023-05-05 12:41:19 +0530

    registering and login done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient, ServerApiVersion } from 'mongodb';
+import mongoose from 'mongoose';
+
 import dotenv from 'dotenv';
 import AuthRoute from './Routes/AuthRoute.js';
 
@@ -15,21 +17,33 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 dotenv.config();
 
 
-const client = new MongoClient(process.env.MONGO_DB, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const client = new MongoClient(process.env.MONGO_DB, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-async function run() {
-  await client.connect()
-    .then(() =>
-      app.listen(process.env.PORT, () =>
-        console.log(`listening on port ${process.env.PORT}`)));
-}
-run().catch(console.dir)
+// async function run() {
+//   await client.connect()
+//     .then(() =>
+//       app.listen(process.env.PORT, () =>
+//         console.log(`listening on port ${process.env.PORT}`)));
+// }
+// run().catch(console.dir)
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error));
 
 // usage of Routes 
 app.use('/auth', AuthRoute);
