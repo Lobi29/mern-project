1cef60e3c8451dcd6ee639082eb7d7fc23a92809
chao lobi <gogoiladitya@gmail.com>
2023-05-05 11:18:00 +0530

    setting up the authentication route

--- server/README.md
+++ server/README.md
@@ -1,1 +1,6 @@
 # backend 
+
+Index.js -> Routes -> Controllers
+
+Routes : routes are paths to reach a specific controller
+Controllers : controllers are just javascript function
--- server/index.js
+++ server/index.js
@@ -1,23 +1,35 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
+import AuthRoute from './Routes/AuthRoute.js';
+
+// Routes
 
 const app = express();
 
+// Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Listening at ${process.env.PORT}`)
-    )
-  )
-  .catch((error) => console.log(error));
+
+const client = new MongoClient(process.env.MONGO_DB, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  await client.connect()
+    .then(() =>
+      app.listen(process.env.PORT, () =>
+        console.log(`listening on port ${process.env.PORT}`)));
+}
+run().catch(console.dir)
+
+// usage of Routes 
+app.use('/auth', AuthRoute);
