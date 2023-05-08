c607321d7ae31fec53ba687615be80ad07fd6313
chao lobi <gogoiladitya@gmail.com>
2023-05-08 12:35:56 +0530

    Post controller and post route is done

--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { createPost, getPost, updatePost, deletePost, likePost } from "../Controllers/PostController.js";
 
 
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-    res.send("Post Route");
-})
-
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.put('/:id/like', likePost);
 export default router;
