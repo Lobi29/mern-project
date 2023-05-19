5d9c28be298ee0249677b7a6f43e1cdc3f012f4e
chao lobi <gogoiladitya@gmail.com>
2023-05-19 21:08:09 +0530

    setting up JWT

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,5 +1,6 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 // Registering a new User
 export const registerUser = async(req, res) => {
@@ -20,10 +21,14 @@ export const registerUser = async(req, res) => {
         if (oldUser) {
             return res.status(400).json({message: "username is already registered!"})
         }
+        const user = await newUser.save();
 
-
-        await newUser.save()
-        res.status(200).json(newUser)
+        const token = jwt.sign(
+            { username: user.username, id: user._id },
+            process.env.JWT_KEY,
+            { expiresIn: "1h"}
+        );
+        res.status(200).json({ user, token });
     } catch ( error ) {
         res.status(500).json({ message: error.message })
     }
@@ -39,7 +44,16 @@ export const loginUser = async (req, res) => {
         if (user) {
             const validity = await bcrypt.compare(password, user.password);
 
-            validity ? res.status(200).json(user) : res.status(400).json("Wrong Password");
+            if (!validity) {
+                res.status(400).json("Wrong Password")
+            } else {
+                const token = jwt.sign(
+                    { username: user.username, id: user._id },
+                    process.env.JWT_KEY,
+                    { expiresIn: "1h"}
+                );
+                res.status(200).json({ user, token })
+            }
         } else {
             res.status(404).json("User does not exists")
         }
